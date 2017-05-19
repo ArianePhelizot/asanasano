ca164b3a5b0f5ba24bcb4eee56162c1473f0b0fd
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-19 19:16:53 +0200

    WIP course policy

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -4,4 +4,21 @@ class CoursePolicy < ApplicationPolicy
       scope
     end
   end
+
+  def create?
+    user_is_group_owner?
+  end
+
+  def update?
+    user_is_group_owner?
+  end
+
+  def destroy?
+    user_is_group_owner?
+  end
+
+  # on teste ici si le user est le créateur du group.
+  def user_is_group_owner?
+    record.group.user == user
+  end
 end
