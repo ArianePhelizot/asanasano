d062702a11843ddcbf39ecc950c5d73b97f06551
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-19 18:01:37 +0200

    migration detailed

--- db/migrate/20170519155032_create_groups.rb
+++ db/migrate/20170519155032_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
+      t.string :name
+      t.references :users, foreign_key: true, index: true
       t.timestamps
     end
   end
