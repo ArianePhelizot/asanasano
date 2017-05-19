7df616a433ba29eab86ec4ef0c8e2baef8a36dc0
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-19 16:34:18 +0200

    courses routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,12 @@
 Rails.application.routes.draw do
-  get 'courses/show'
-
-  get 'courses/new'
-
-  get 'courses/create'
-
-  get 'courses/edit'
-
-  get 'courses/update'
-
-  get 'courses/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Attachinary::Engine => "/attachinary"
   devise_for :users
   root to: 'pages#home'
   resources :groups, only: [:new, :create, :edit, :update, :destroy] do
     #we nest course routes under group routes because we need group_id to create a course
-    resources :courses, only: [:new, :create, :edit, :update, :destroy]
+    resources :courses, only: [:new, :create, :edit, :update]
   end
+  resources :courses, only: [:show, :destroy]
   get 'dashboard', to: 'pages#dashboard'
 end
