2699bd74456924a90873bd9333ebd0a0e2379455
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-19 17:34:06 +0200

    courses#new and courses#create done

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,12 +1,24 @@
 class CoursesController < ApplicationController
+  # on a besoin de group pour créer un course
+  before_action :find_group, only: [:new, :create]
+  # before_action :find_course, only: []
+
   def show
     @course = Course.find(params[:id])
   end
 
   def new
+    @course = Course.new
   end
 
   def create
+    @course = Course.new(params(course_params))
+    @course.group = @group
+    if @course.save
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,4 +29,21 @@ class CoursesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :sport_id, :address, :meeting_point,
+                                   :capacity_max,
+                                   :details,
+                                   :coach_id)
+  end
+
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
+
+  # def find_course
+  #   @course = Course.find(params[:id])
+  # end
 end
