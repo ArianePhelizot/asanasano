81ff8c87d6603ab9ec063f134cc84d58e405aab3
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-19 12:44:40 +0200

    groups#update + find_group method done

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,12 @@
 class GroupsController < ApplicationController
+  before_action :find_group, only: [:edit, :update]
+
   def new
     @group = Group.new
   end
 
   def create
-    @group = Group.new(params[])
+    @group = Group.new(params(group_params))
     @group.user = current_user
     if @group.save
       redirect_to dashboard_path
@@ -17,12 +19,22 @@ class GroupsController < ApplicationController
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(params(group_params))
+      redirect_to dashboard_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
 
   private
+
+  def find_group
+    @group = Group.find(params[:id])
+  end
 
   def group_params
     params.require(:group).permit(:name, :photo)
