179108a04d775771b4e1d8279a1eef08269d940d
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-19 18:10:14 +0200

    plus validation on owner_id presence

--- app/models/group.rb
+++ app/models/group.rb
@@ -16,5 +16,6 @@
 class Group < ApplicationRecord
    validates :name, presence: true, length: { maximum: 20 }
    belongs_to :user, foreign_key: "owner_id"
+   validates :owner_id, presence: true
 end
 
