533f6fa6713caa8ddaf939755aac71055674194c
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-19 17:52:11 +0200

    same player play again on saves forget....+ Group model generated

---
+++ app/models/sport.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: sports
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Sport < ApplicationRecord
+  has_attachment :photo
+  has_attachment :icon
+  has_and_belongs_to_many :coaches
+
+  validates :name, :photo, :icon, presence: true
+  validates :description, length: { maximum: 500 }
+
+end
