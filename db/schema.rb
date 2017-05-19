27f5b1e5f3d9153fce9730dc7a19f349aa77f3e4
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-19 12:03:36 +0200

    Synchro schema.rb (orders table was removed)

--- db/schema.rb
+++ db/schema.rb
@@ -36,15 +36,6 @@ ActiveRecord::Schema.define(version: 20170519084756) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string   "state"
-    t.string   "teddy_sku"
-    t.integer  "amount_cents", default: 0, null: false
-    t.json     "payment"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
