d0625ef6cc0f77aea9cdc08f928ecfd6ed3393ae
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-20 18:15:28 +0200

    Slot migration tuning

--- db/migrate/20170520155919_create_slots.rb
+++ db/migrate/20170520155919_create_slots.rb
@@ -2,7 +2,21 @@ class CreateSlots < ActiveRecord::Migration[5.0]
   def change
     create_table :slots do |t|
 
+      t.date :date
+      t.time :start_time
+      t.time :end_time
+
+
+      t.integer :participants_min
+      t.monetize :price
+
+      t.string :specificities
+      t.integer :status, default: 0
+
+      t.references :course, foreign_key: true, index: true
+
       t.timestamps
     end
   end
 end
+
