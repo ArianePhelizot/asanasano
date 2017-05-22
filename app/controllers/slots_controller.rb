c2a934f13ff73622143b675381b57fe4b00d4016
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-22 11:07:25 +0200

    slots#new and slots#create added

---
+++ app/controllers/slots_controller.rb
@@ -0,0 +1,38 @@
+class SlotsController < ApplicationController
+  before_action :find_course, only: [:new, :create]
+
+  def new
+    @slot = Slot.new
+  end
+
+  def create
+    @slot = Slot.new(slot_params)
+    @slot.course = @course
+    if @slot.save
+      redirect_to course_path(@course)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def slot_params
+    params.require(:slot).permit(:capacity_min,
+                                 :date,
+                                 :start_time_hours,
+                                 :start_date_minutes
+                                 :end_time_hours
+                                 :end_time_minutes)
+  end
+
+  def find_course
+    @course = course.find(params[:course_id])
+  end
+end
