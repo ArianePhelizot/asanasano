3cd6bf2384ab568398baa6063a3ef13b47dd1f4f
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-24 17:42:14 +0200

    Route modification

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # ... et non dans leur propre show
   resources :groups, only: [:new, :create, :edit, :update, :destroy] do
     # On nest ces routes car on a besoin de group_id pour new, create, edit et update
-    resources :courses, only: [:new, :create, :edit, :update, :publish]
+    resources :courses, only: [:new, :create, :edit, :update]
+    get 'publish', to: 'courses#publish'
   end
 
   resources :courses, only: [:show, :destroy] do
