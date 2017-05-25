6d71449706a6f5cebe4c3244c9f733a3a18b3805
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-25 22:19:33 +0200

    New user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path
+    else
+      render :edit_profile
+    end
+  end
+
+private
+
+  def set_user
+    @user = current_user
+    authorize @user
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name,
+                                   :last_name,
+                                   :email,
+                                   :phone_number,
+                                   :photo
+                                   )
+  end
+
+end
