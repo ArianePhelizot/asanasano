5bd5b582c095d619a971bd37d5daffe0caff8029
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-25 22:20:33 +0200

    New methods ein Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @user = current_user
   end
 
   def dashboard
     @user = current_user
   end
+
+  def profile
+    @user = current_user
+  end
+
+  def edit_profile
+    @user = current_user
+  end
+
 end
