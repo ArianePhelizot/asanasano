18af91fc6e818a063b0bb7a707e7fae8b4a6ee1a
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-28 17:12:50 +0200

    Coach info update

---
+++ app/policies/coach_policy.rb
@@ -0,0 +1,11 @@
+class CoachPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def update?
+      record.user == user # seul le coach peut modifier son profil de coach
+    end
+end
