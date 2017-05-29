c4423ce2d515f8f3df66db7903a7050799999d7d
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-29 07:28:11 +0200

    Controller test - problem

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -7,8 +7,11 @@ before_action :set_coach
   end
 
   def update
+    binding.pry
     if @coach.update(coach_params)
+      @coach.sports = sports_update
       redirect_to profile_path
+
     else
       render :edit
     end
@@ -22,7 +25,11 @@ private
   end
 
   def coach_params
-      params.require(:coach).permit(:description)
+      params.require(:coach).permit(:description, :sport_ids)
+  end
+
+  def sports_update
+    params["sport_ids"].delete_at[0].map {|id| id.to_i}
   end
 
 end
