5c148c94264482be4e0e40df8c93b36f763e9934
Guillaume Jauffret <guillaumejfrt@gmail.com>
2017-05-30 18:19:28 +0200

    done

--- app/assets/javascripts/scroll_to_slot_on_create.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("#slot-submit-btn").click(function() {
-      $('html, body').animate({
-          scrollTop: $("#slot-card-71").offset().top
-      }, 'fast');
-  });
-});
