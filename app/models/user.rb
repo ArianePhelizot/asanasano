df58b7e29607bb0e9437f3a35b109711e5da7d2d
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-06-12 17:25:40 +0200

    Customizing User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
-#  sign_in_count          :integer          default("0"), not null
+#  sign_in_count          :integer          default(0), not null
 #  current_sign_in_at     :datetime
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :inet
@@ -24,12 +24,23 @@
 #  facebook_picture_url   :string
 #  token                  :string
 #  token_expiry           :datetime
-#  admin                  :boolean          default("false"), not null
+#  admin                  :boolean          default(FALSE), not null
+#  invitation_token       :string
+#  invitation_created_at  :datetime
+#  invitation_sent_at     :datetime
+#  invitation_accepted_at :datetime
+#  invitation_limit       :integer
+#  invited_by_type        :string
+#  invited_by_id          :integer
+#  invitations_count      :integer          default(0)
 #
 # Indexes
 #
 #  index_users_on_coach_id              (coach_id)
 #  index_users_on_email                 (email) UNIQUE
+#  index_users_on_invitation_token      (invitation_token) UNIQUE
+#  index_users_on_invitations_count     (invitations_count)
+#  index_users_on_invited_by_id         (invited_by_id)
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 
