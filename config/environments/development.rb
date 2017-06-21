ca0c2f24ff901064b2817b12c1bd5f1a19e1e905
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-06-21 20:05:34 +0200

    Letter opener follow-up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Lines added according to Le Wagon course on transactional emails.
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -53,3 +57,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
