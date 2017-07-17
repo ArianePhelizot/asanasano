4fe226f933390ac7dd30bac142036dd98933edf5
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-07-17 18:16:24 +0200

    rails db:migrate RAILS_ENV=test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170715142305) do
+ActiveRecord::Schema.define(version: 20170612150007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,10 +46,8 @@ ActiveRecord::Schema.define(version: 20170715142305) do
 
   create_table "coaches", force: :cascade do |t|
     t.text     "description"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "experience"
-    t.text     "languages",   default: [],              array: true
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "coaches_groups", id: false, force: :cascade do |t|
