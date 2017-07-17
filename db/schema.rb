81d333f48895c5a5a478a89feef6501cb285c520
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-07-17 18:08:01 +0200

    Scheme update…but why now?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612150007) do
+ActiveRecord::Schema.define(version: 20170715142305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,8 +46,10 @@ ActiveRecord::Schema.define(version: 20170612150007) do
 
   create_table "coaches", force: :cascade do |t|
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "experience"
+    t.text     "languages",   default: [],              array: true
   end
 
   create_table "coaches_groups", id: false, force: :cascade do |t|
