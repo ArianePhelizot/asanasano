7cff5a6cf05ae200a17f73b947afc2fccdb6c4a9
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-07-26 17:22:14 +0200

    Overiding invite_resource in order to encompass every possibility: user already known (already in the group or not)

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,10 +1,38 @@
 class Users::InvitationsController < Devise::InvitationsController
 
-  def invite_resource
-    # group tracking
-    super do |u|
-      u.groups.push(find_group)
+  # def invite_resource
+  #   # group tracking
+  #   super do |u|
+  #     u.groups.push(find_group)
+  #   end
+  # end
+
+  def invite_resource(&block)
+
+    # je regarde si je connais l'adresse email
+    @user = User.find_by(email: invite_params[:email])
+
+    # si le user existe et n'appartient pas déjà au groupe
+    if @user && !@user.groups.include?(find_group)
+      @user.groups.push(find_group)
+      # invite! instance method returns a Mail::Message instance
+      @user.invite!(current_user)
+      # return the user instance to match expected return type
+      @user
+
+    # si le user existe et appartient déjà au groupe
+    elsif @user && @user.groups.include?(find_group)
+      # comportement par défaut mais avec une custimization à prévoir du message d'erreur (vs email déjà pris)
+      super
+
+    # si le user n'existe pas
+    else
+      # comportement classique + rajout au groupe
+      super do |u|
+        u.groups.push(find_group)
+      end
     end
+
   end
 
 
