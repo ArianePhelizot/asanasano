e3f90f0ca9deed40900e47ed7463e953f30d7b57
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-07-26 15:07:08 +0200

    Consequences of strategy change tu add invite to a specific group using invite_resource vs create

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,41 +1,10 @@
 class Users::InvitationsController < Devise::InvitationsController
 
-def invite_resource
+  def invite_resource
     # group tracking
     super do |u|
       u.groups.push(find_group)
     end
-  end
-
-
-
-
-  # POST /resource/invitation
-  def create
-
-
-    #invite_resource déclenche l'envoi du mail. Comment du coup à la création, prendre en compte l'udate des groupes
-    self.resource = invite_resource
-
-    resource_invited = resource.errors.empty?
-
-    yield resource if block_given?
-
-    if resource_invited
-
-      if is_flashing_format? && self.resource.invitation_sent_at
-        set_flash_message :notice, :send_instructions, :email => self.resource.email
-      end
-      if self.method(:after_invite_path_for).arity == 1
-        respond_with resource, :location => after_invite_path_for(current_inviter)
-      else
-        respond_with resource, :location => after_invite_path_for(current_inviter, resource)
-      end
-
-    else
-      respond_with_navigational(resource) { render :new }
-    end
-
   end
 
 
