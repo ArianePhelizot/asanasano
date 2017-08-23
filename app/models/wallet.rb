1a142a8989bb748a31fbdc38f0cbf2a377b8c2cf
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-08-23 12:01:02 +0200

    Wallet model creation

---
+++ app/models/wallet.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: wallets
+#
+#  id          :integer          not null, primary key
+#  account_id  :integer
+#  tag         :string
+#  mangopay_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_wallets_on_account_id  (account_id)
+#
+
+class Wallet < ApplicationRecord
+  belongs_to :user
+end
