b7410bb197ef30a5a4442129ac6c09d1cec56db9
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-08-27 21:11:50 +0200

    Duplicate info identified and deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,11 +30,4 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
-  def default_url_option
-    if Rails.env.production?
-      {:host => "asanasano.com/"}
-    else
-      {:host => "http://localhost:3000/"}
-    end
-  end
 end
