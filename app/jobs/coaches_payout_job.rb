a71582a3b48c6f9ccc2cf0c84b7c01433198dbdc
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-09-04 11:38:12 +0200

    Coaches Payout WIP

---
+++ app/jobs/coaches_payout_job.rb
@@ -0,0 +1,51 @@
+class CoachesPayoutJob < ApplicationJob
+  queue_as :default
+
+# Parametring the payment period from slot date to coach payout date
+COACH_PAYOUT_PERIOD = 3 #days
+
+ASANASANO_FEES_RATE_ON_PAYOUTS = 0.1
+
+  def perform
+    # Je veux virer l'argent du wallet des profs sur leur compte en banque pour
+    # les cours qui ont eu lieu, il y a 3 jours
+
+    # Liste de toutes les séances d'il y a 3 jours
+    slots = Slot.all.select {|slot| slot.date == Date.today - 3.days}
+    slots = slots.select {|slot| slot.status == "passed"}
+
+    # Je regarde pour chacune de ces séances, quel était le coach
+    slots.each do |slot|
+      coach_user = slot.course.coach.user
+      if coach_user.account.present? && coach_user.account.iban.present?
+        # je regarde quelle est la somme associée aux orders settled du slots
+        paid_orders = slot.orders.select { |order| order.state == "paid"}
+        paid_and_settled_orders = slot.orders.select { |order| order.settled = true}
+        billed_amounts_array = paid_and_settled_orders.map { |order| order.amount_cents}
+        billed_sum_cents = billed_amounts_array.reduce(:+)
+
+        # et je fais le virement correspondant au prof
+
+        mangopay_payout = MangoPay::PayOut.create(
+        "Tag": "Payout",
+        "AuthorId": ENV["MANGOPAY_CLIENT_ID"],
+        "DebitedFunds": {
+          "Currency": "EUR",
+          "Amount": billed_sum_cents * (1 - ASANASANO_FEES_RATE_ON_PAYOUTS)
+          },
+        "Fees": {
+          "Currency": "EUR",
+          "Amount": billed_sum_cents * (1 - ASANASANO_FEES_RATE_ON_PAYOUTS)
+          },
+        "BankAccountId": coach_user.account.iban.mangopay_id,
+        "DebitedWalletId": coach_user.account.mangopay_id,
+        "BankWireRef": "Séance #{slot.course.name} du #{slot.date.strftime('%v')}"
+        )
+      end
+    end
+  end
+end
+
+
+
+
