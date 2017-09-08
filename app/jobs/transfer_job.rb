74f8e255d0ab54721b96b72a80fb95bb91b11808
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-09-08 17:27:34 +0200

    Logging Mangopay transfer creation in mango pay logs

--- app/jobs/transfer_job.rb
+++ app/jobs/transfer_job.rb
@@ -12,7 +12,7 @@ class TransferJob < ApplicationJob
     # for all new "passed" slots, i.e for "passed" slots
     # whose "paid" orders haven't been "settled" yet
 
-    # selection of all passed slots, paid but non stelled orders
+    # selection of all passed slots with paid but not yet settled orders
     paid_orders = Order.all.where(state: "paid")
     paid_orders_of_passed_slots = paid_orders.select { |order| order.slot.status == "passed" }
     # rubocop:disable LineLength
@@ -24,30 +24,28 @@ class TransferJob < ApplicationJob
       user = order.user
       coach_user = slot.course.coach.user
       # check first that customer did not cancell his attendance to slot
-      next unless slot.users.include?(user)
-      # if yes ask for transferring money from customer wallet to coach walle
+      if slot.users.include?(user)
+      # if yes ask for transferring money from customer wallet to coach wallet
 
 
-      begin
-        mangopay_transfer = MangoPay::Transfer.create(
-          "Tag": "Money transfer",
-          # "Tag": "Money transfer from user_id:#{user.id}
-          # to coach user_id:#{coach_user.id}, slot_id: #{slot.id}",
-          # "AuthorId": ENV["MANGOPAY_CLIENT_ID"], author has to be the wallet owner!!!
-          "AuthorId": user.account.mangopay_id,
-          "CreditedUserId": user.account.mangopay_id,
-          "DebitedFunds": { "Currency": "EUR",
-                            "Amount": order.amount_cents * (1 - ASANASANO_FEES_RATE_ON_WALLET_TRANSFER) },
-          "Fees": { "Currency": "EUR",
-                    "Amount": order.amount_cents * ASANASANO_FEES_RATE_ON_WALLET_TRANSFER },
-          "DebitedWalletId": user.account.wallet.mangopay_id,
-          "CreditedWalletId": coach_user.account.wallet.mangopay_id
-        )
+        begin
+          mangopay_transfer = MangoPay::Transfer.create(
+            "Tag": "Money transfer",
+            # "Tag": "Money transfer from user_id:#{user.id}
+            # to coach user_id:#{coach_user.id}, slot_id: #{slot.id}",
+            # "AuthorId": ENV["MANGOPAY_CLIENT_ID"], author has to be the wallet owner!!!
+            "AuthorId": user.account.mangopay_id,
+            "CreditedUserId": user.account.mangopay_id,
+            "DebitedFunds": { "Currency": "EUR",
+                              "Amount": order.amount_cents * (1 - ASANASANO_FEES_RATE_ON_WALLET_TRANSFER) },
+            "Fees": { "Currency": "EUR",
+                      "Amount": order.amount_cents * ASANASANO_FEES_RATE_ON_WALLET_TRANSFER },
+            "DebitedWalletId": user.account.wallet.mangopay_id,
+            "CreditedWalletId": coach_user.account.wallet.mangopay_id
+          )
 
-        # ATTENTION IL FAUDRAIT ICI QUE J'INTEGRE LA REPONSE DE MANGOPAY
-        # update order state
-        order.settled = true
-        order.save
+          order.transfer_mangopay_id = mangopay_transfer["Id"]
+          order.save
 
         rescue MangoPay::ResponseError => ex
           log_error = ex.message
@@ -58,8 +56,8 @@ class TransferJob < ApplicationJob
                             mangopay_answer: mangopay_transfer,
                             user_id: user.id.to_i,
                             error_logs: log_error)
+        end
       end
-    end
   end
   # rubocop:enable Metrics/AbcSize
   # rubocop:enable Metrics/MethodLength
