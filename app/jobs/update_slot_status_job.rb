751a375a617c5862a45a7272432f3aaa35d52536
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-09-11 19:10:38 +0200

    Pas à pas

--- app/jobs/update_slot_status_job.rb
+++ app/jobs/update_slot_status_job.rb
@@ -3,19 +3,24 @@ class UpdateSlotStatusJob < ApplicationJob
 
   def perform
     # Selection of all slots with both a due slot date
-    # and a "created" or "confirmed status", i.e not "cancelled"
+    # and a "created" or "confirmed status", i.e not "cancelled", nor "archived"
     passed_slots = Slot.all.select { |slot|
       slot.date.past?
     }
 
-    passed_and_not_cancelled_slots = passed_slots.select { |slot|
-      slot.status == "created" || "confirmed"
-    }
-
-    # We the change the status of those slots to "passed"
-    passed_and_not_cancelled_slots.each do |slot|
+    #unable to factorize this code properly using .select and ||!!
+    passed_slots_created = passed_slots.select { |slot| slot.status == "created"}
+    passed_slots_created.each do |slot|
       slot.status = "passed"
       slot.save
     end
+
+
+    passed_slots_confirmed = passed_slots.select { |slot| slot.status == "confirmed"}
+    passed_slots_confirmed.each do |slot|
+      slot.status = "passed"
+      slot.save
+    end
+
   end
 end
