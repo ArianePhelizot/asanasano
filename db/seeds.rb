da9270b3058afe4b0cf0e97ef8b2f98390f899f8
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-09-14 19:18:50 +0200

    Updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,12 @@ Group.destroy_all
 User.destroy_all
 Coach.destroy_all
 Sport.destroy_all
+Account.destroy_all
+Wallet.destroy_all
+Iban.destroy_all
+Order.destroy_all
 
-puts 'Creating users, groups, coaches, courses and sports...'
+puts 'Creating users, groups, coaches, courses and sports, and accounts, wallets, ibans, slots...'
 
 # SPORT SEEDS
 
@@ -43,9 +47,9 @@ puts "#{Sport.count} sports created"
 
 # COACH SEEDS
 
-coach1 = Coach.create!(description: 'Je pratique le yoga depuis 25 ans. Je suis à fonds pour vous faire partager ma passion')
-coach2 = Coach.create!(description: 'Diplomée en 2010, je donne des cours collectifs et particuliers de yoga hatha, vinyasa et nidra. J accompagne aussi bien débutants que confirmés')
-coach3 = Coach.create!(description: 'Couteau suisse, fan de sport')
+coach1 = Coach.create!(description: 'Je pratique le yoga depuis 25 ans. Je suis à fonds pour vous faire partager ma passion', experience: 5)
+coach2 = Coach.create!(description: 'Diplomée en 2010, je donne des cours collectifs et particuliers de yoga hatha, vinyasa et nidra. J accompagne aussi bien débutants que confirmés', experience: 6)
+coach3 = Coach.create!(description: 'Couteau suisse, fan de sport', experience: 3)
 
 puts "#{Coach.count} coaches created"
 
@@ -57,33 +61,44 @@ coach3.sports = [sport6, sport10, sport5]
 
 # USER SEEDS
 
+def open_photo(filename)
+  File.open(Rails.root.join('db', 'photos', filename))
+end
+
+
 user1 = User.create!(first_name: 'Ariane',
                      last_name: 'PHELIZOT',
                      email: 'ariane.phelizot@gmail.com',
                      password: 'azerty',
                      phone_number: '0613990061',
+                     photo: open_photo('ariane_photo.png'),
                      admin: true)
 
 user2 = User.create!(first_name: 'Cherine',
                      last_name: 'ELFADEL',
                      email: 'cherineelf@gmail.com',
                      password: 'azerty',
+                     photo: open_photo('cherine_photo.png'),
                      phone_number: '0671991204')
 
 user3 = User.create!(first_name: 'Faustin',
                      last_name: 'VEYSSIERE',
                      email: 'faustin.veyssiere@gmail.com',
                      password: 'azerty',
+                     photo: open_photo('faustin_photo.png'),
                      phone_number: '0782095662')
 
 user4 = User.create!(first_name: 'Guillaume',
                      last_name: 'JAUFFRET',
                      email: 'guillaume.jauffret@gmail.com',
                      password: 'azerty',
+                     photo: open_photo('guillaume_photo.png'),
                      phone_number: '0665647375')
+
 user5 = User.create!(first_name: 'Isabelle',
                      last_name: 'YVAS',
                      email: 'isabelle.ivas@gmail.com',
+                     photo: open_photo('isabelle_photo.jpeg'),
                      password: 'azerty',
                      phone_number: '0601020304',
                      coach: coach1)
@@ -93,6 +108,7 @@ user6 = User.create!(first_name: 'Veronica',
                      email: 'veronica.obama@gmail.com',
                      password: 'azerty',
                      phone_number: '0613990061',
+                     photo: open_photo('veronica_photo.jpg'),
                      coach: coach2)
 
 user7 = User.create!(first_name: 'Mathieu',
@@ -100,6 +116,7 @@ user7 = User.create!(first_name: 'Mathieu',
                      email: 'mathieu.bonfils@gmail.com',
                      password: 'azerty',
                      phone_number: '0616741821',
+                     photo: open_photo('mathieu_photo.png'),
                      coach: coach3)
 
 puts "#{User.count} users created"
@@ -108,9 +125,9 @@ puts 'Associating users and coaches'
 
 # GROUPS SEEDS
 
-group1 = Group.create!(owner_id: user1.id, name: 'Les petites cerises')
-group2 = Group.create!(owner_id: user2.id, name: 'Les grosses fraises')
-group3 = Group.create!(owner_id: user2.id, name: 'Les jeunes courgettes')
+group1 = Group.create!(owner_id: user1.id, name: 'Le Wagon Marseille')
+group2 = Group.create!(owner_id: user2.id, name: 'Le Wagon')
+group3 = Group.create!(owner_id: user2.id, name: 'Happy in Paris')
 
 puts "#{Group.count} groups created"
 
@@ -127,7 +144,7 @@ course1 = Course.create!(name: 'Yoga Hatha',
                          coach_id: coach2.id,
                          sport_id: sport1.id)
 
-course2 = Course.create!(name: 'Préparation Marseille Cassis 2017',
+course2 = Course.create!(name: 'Mars-Cass 2017',
                          content: 'Entrainement collectif pour être prêt le 29 octobre 2017: être en condition et progresser sans se blesser pour être prêt le jour J ',
                          details: 'Tennis de rigueur!',
                          address: 'Rue Joseph Biaggi, 13003 Marseille, France',
@@ -175,68 +192,301 @@ puts "#{Course.count} courses created"
 
 puts 'Associating groups and users'
 
-group1.users = [user1, user2, user3, user4]
+group1.users = [user1, user2, user3, user4, user5, user7]
 group1.save
-group2.users = [user1, user2]
+group2.users = [user1, user2, user6]
 group2.save
-group3.users = [user1, user4]
+group3.users = [user1, user4, user6, user4]
 group3.save
 
 puts 'Associating coaches and groups'
 
 group1.coaches = [coach1, coach2, coach3]
 group2.coaches = [coach3]
+group3.coaches = [coach1, coach3]
+
+# ACCOUNT SEEDS
+
+account1 = Account.create!(user_id: user1.id,
+                          tag: "User_id: " + user1.id.to_s,
+                          person_type: "NATURAL",
+                          birthday: Date.new(1975, 12, 26),
+                          address_line1: "10 rue Oberkampf",
+                          postal_code: "75011",
+                          city: "PARIS",
+                          region: "",
+                          country_of_residence: "FR",
+                          nationality: "FR")
+
+account2 = Account.create!(user_id: user2.id,
+                          tag: "User_id: " + user2.id.to_s,
+                          person_type: "NATURAL",
+                          birthday: Date.new(1998, 9, 5),
+                          address_line1: "4 rue de la Cannebière",
+                          postal_code: "13002",
+                          city: "MARSEILLE",
+                          region: "",
+                          country_of_residence: "FR",
+                          nationality: "FR")
+
+account3 = Account.create!(user_id: user3.id,
+                          tag: "User_id: " + user3.id.to_s,
+                          person_type: "NATURAL",
+                          birthday: Date.new(1997, 2, 27),
+                          address_line1: "6 avenue Mac Mahon",
+                          postal_code: "75008",
+                          city: "PARIS",
+                          region: "",
+                          country_of_residence: "FR",
+                          nationality: "FR")
+
+account4 = Account.create!(user_id: user4.id,
+                          tag: "User_id: " + user4.id.to_s,
+                          person_type: "NATURAL",
+                          birthday: Date.new(1987, 6, 15),
+                          address_line1: "66 rue de Longchamp",
+                          postal_code: "75016",
+                          city: "PARIS",
+                          region: "",
+                          country_of_residence: "FR",
+                          nationality: "FR")
+
+
+account5 = Account.create!(user_id: user5.id,
+                          tag: "User_id: " + user5.id.to_s + "/Coach_id: " + coach1.id.to_s,
+                          person_type: "NATURAL",
+                          birthday: Date.new(1971, 11, 30),
+                          address_line1: "2 rue Eugène Carrière",
+                          postal_code: "75018",
+                          city: "PARIS",
+                          region: "",
+                          country_of_residence: "FR",
+                          nationality: "FR")
+
+account6 = Account.create!(user_id: user6.id,
+                          tag: "User_id: " + user6.id.to_s + "/Coach_id: " + coach2.id.to_s,
+                          person_type: "NATURAL",
+                          birthday: Date.new(1993, 5, 8),
+                          address_line1: "15 boulevard de Grenelle",
+                          postal_code: "75015",
+                          city: "PARIS",
+                          region: "",
+                          country_of_residence: "FR",
+                          nationality: "FR")
+
+account7 = Account.create!(user_id: user7.id,
+                          tag: "User_id: " + user7.id.to_s + "/Coach_id: " + coach3.id.to_s,
+                          person_type: "NATURAL",
+                          birthday: Date.new(1995, 7, 11),
+                          address_line1: "165 rue de Paradis",
+                          postal_code: "75002",
+                          city: "MARSEILLE",
+                          region: "",
+                          country_of_residence: "FR",
+                          nationality: "FR")
+
+  Account.all.each do |account|
+        mangopay_user = MangoPay::NaturalUser.create(
+        "Tag": account.tag,
+        "Email": account.user.email,
+        "FirstName": account.first_name,
+        "LastName": account.last_name,
+        # Pb de format quand champ vide car il me faut a minima un caractère
+        "Address":
+        { "AddressLine1": account.address_line1,
+          "AddressLine2": account.address_line2,
+          "City": account.city,
+          "Region": account.region,
+          "PostalCode": account.postal_code,
+          "Country": account.country_of_residence },
+        "Birthday": account.birthday.to_time.to_i,
+        "Nationality": account.nationality,
+        "CountryOfResidence": account.country_of_residence
+      )
+
+      account.mangopay_id = mangopay_user["Id"]
+      account.save
+
+      MangopayLog.create(event: "natural_account_creation", mangopay_answer: mangopay_user, user_id: account.user.id.to_i )
+  end
+
+puts "#{Account.count} accounts created"
+
+
+# WALLET SEEDS
+
+  Account.all.each do |account|
+
+    wallet = Wallet.create(account_id: account.id, tag: account.tag)
+
+      # Wallet creation by Mangpay
+      mangopay_wallet = MangoPay::Wallet.create(
+        "Tag": wallet.tag,
+        "Owners": [Account.find(wallet.account_id).mangopay_id], # mangopay_id de l'account
+        "Description": "ASANASANO Wallet",
+        "Currency": "EUR"
+      )
+
+      # Recuperation of the Mangopay Id created for the wallet
+      wallet.mangopay_id = mangopay_wallet["Id"]
+      wallet.save
+
+
+      MangopayLog.create(event: "wallet_creation",
+                        mangopay_answer: mangopay_wallet,
+                        user_id: account.user.id.to_i)
+  end
+
+puts "#{Wallet.count} wallets created"
+
+
+# IBAN SEEDS
+
+iban1 = Iban.create(account_id: user5.account.id, iban: "FR7630001007941234567890185")
+iban2 = Iban.create(account_id: user6.account.id, iban: "FR7630004000031234567890143" )
+iban3 = Iban.create(account_id: user7.account.id, iban: "FR7630006000011234567890189")
+
+  Iban.all.each do |iban|
+
+     # créer l'IBAN chez Mangopay
+      mangopay_iban_bankaccount = MangoPay::BankAccount.create(
+        iban.account.user.account.mangopay_id,
+        { Type: "IBAN",
+      OwnerName: [iban.account.user.first_name, iban.account.user.last_name].join(" "),
+      OwnerAddress: iban.account.user.account.address,
+      IBAN: iban.account.user.account.iban.iban,
+      Tag: "Bank account of #{iban.account.user.account.iban.tag}" }
+      )
+
+      # updater l'IBAN avec les données de MangoPay
+      iban.mangopay_id = mangopay_iban_bankaccount["Id"]
+      iban.active = mangopay_iban_bankaccount["Active"]
+      iban.save
+
+      MangopayLog.create(event: "iban_creation",
+                          mangopay_answer: mangopay_iban_bankaccount,
+                          user_id: iban.account.user.id.to_i)
+  end
+
+puts "#{Iban.count} ibans created"
 
 # SLOT SEEDS
 
-# slot1 = Slot.create!(date:
-#                     start_at:
-#                     end_at:
-#                     specificities:
-#                     participants_min:
-#                     price_cents:
-#                     status:
-#                     course_id:
+# for index in 0...(musicians.size)
+#   musician = musicians[index]
+#   puts "#{index + 1} - #{musician}"
+# end
+
+for i in -7..7
+  Slot.create!(date: Date.today + i,
+            start_at: DateTime.new((Date.today+i).year, (Date.today+i).month, (Date.today+i).day, 12, 00),
+            end_at: DateTime.new((Date.today+i).year, (Date.today+i).month, (Date.today+i).day, 12, 45),
+            specificities: "Des matelas seront sur place! Pensez juste à prendre des tenues confortables",
+            participants_min: 5,
+            price_cents: 1000,
+            status: 0,
+            course_id: course1.id
+            )
+end
+
+
+def date_of_next(day)
+  date  = Date.parse(day)
+  delta = date > Date.today ? 0 : 7
+  date + delta
+end
+
+
+for i in -4..4
+  Slot.create!(date: date_of_next("Tuesday") + 7*i,
+              start_at: DateTime.new(date_of_next("Tuesday").year, date_of_next("Tuesday").month, date_of_next("Tuesday").day, 18, 00),
+              end_at: DateTime.new(date_of_next("Tuesday").year, date_of_next("Tuesday").month, date_of_next("Tuesday").day, 19, 15),
+              specificities: "Prévoyez juste de bonnes baskets!",
+              participants_min: 3,
+              price_cents: 500,
+              status: 0,
+              course_id: course2.id
+              )
+end
+
+
+
+for i in -4..4
+  Slot.create!(date: date_of_next("Thursday") + 7*i,
+              start_at: DateTime.new(date_of_next("Tuesday").year, date_of_next("Tuesday").month, date_of_next("Tuesday").day, 13, 00),
+              end_at: DateTime.new(date_of_next("Tuesday").year, date_of_next("Tuesday").month, date_of_next("Tuesday").day, 14, 00),
+              specificities: "Prévoyez juste de bonnes baskets!",
+              participants_min: 3,
+              price_cents: 500,
+              status: 0,
+              course_id: course5.id
+              )
+end
+
+
+for i in -4..4
+  Slot.create!(date: date_of_next("Wednesday") + 7*i,
+              start_at: DateTime.new(date_of_next("Tuesday").year, date_of_next("Tuesday").month, date_of_next("Tuesday").day, 13, 00),
+              end_at: DateTime.new(date_of_next("Tuesday").year, date_of_next("Tuesday").month, date_of_next("Tuesday").day, 14, 00),
+              specificities: "Merci de venir 5 minutes avant le début de la séance pour que nous puissions commencer à l'heure",
+              participants_min: 6,
+              price_cents: 1500,
+              status: 0,
+              course_id: course4.id
+              )
+end
+
+
+
+
+# slot1 = Slot.create!(date: Date.today + 1,
+#                     start_at: DateTime.new((Date.today+1).year, (Date.today+1).month, (Date.today+1).day, 12, 00),
+#                     end_at: DateTime.new((Date.today+1).year, (Date.today+1).month, (Date.today+1).day, 13, 00),
+#                     specificities: "Des matelas seront sur place! Pensez juste à prendre des tenues confortables",
+#                     participants_min: 5,
+#                     price_cents: 10,
+#                     status: 0,
+#                     course_id: course1.id
 #                     )
 
-# slot2 = Slot.create!(date:
-#                     start_at:
-#                     end_at:
-#                     specificities:
-#                     participants_min:
-#                     price_cents:
-#                     status:
-#                     course_id:
+# slot2 = Slot.create!(date: Date.today + 2,
+#                     start_at: DateTime.new((Date.today+2).year, (Date.today+2).month, (Date.today+2).day, 12, 00),
+#                     end_at: DateTime.new((Date.today+2).year, (Date.today+2).month, (Date.today+2).day, 13, 00),
+#                     specificities: "Des matelas seront sur place! Pensez juste à prendre des tenues confortables",
+#                     participants_min: 5,
+#                     price_cents: 10,
+#                     status: 0,
+#                     course_id: course1.id
 #                     )
 
-# slot3 = Slot.create!(date:
-#                     start_at:
-#                     end_at:
-#                     specificities:
-#                     participants_min:
-#                     price_cents:
-#                     status:
-#                     course_id:
+# slot3 = Slot.create!(date: Date.today + 3,
+#                     start_at: DateTime.new((Date.today+3).year, (Date.today+3).month, (Date.today+3).day, 12, 00),
+#                     end_at: DateTime.new((Date.today+3).year, (Date.today+3).month, (Date.today+3).day, 13, 00),
+#                     specificities: "Des matelas seront sur place! Pensez juste à prendre des tenues confortables",
+#                     participants_min: 5,
+#                     price_cents: 10,
+#                     status: 0,
+#                     course_id: course1.id
 #                     )
 
-# slot4 = Slot.create!(date:
-#                     start_at:
-#                     end_at:
-#                     specificities:
-#                     participants_min:
-#                     price_cents:
-#                     status:
-#                     course_id:
+# slot4 = Slot.create!(date: Date.today + 4,
+#                     start_at: DateTime.new((Date.today+4).year, (Date.today+4).month, (Date.today+4).day, 12, 00),
+#                     end_at: DateTime.new((Date.today+4).year, (Date.today+4).month, (Date.today+4).day, 13, 00),
+#                     specificities: "Des matelas seront sur place! Pensez juste à prendre des tenues confortables",
+#                     participants_min: 5,
+#                     price_cents: 10,
+#                     status: 0,
+#                     course_id: course1.id
 #                     )
 
-# slot5 = Slot.create!(date:
-#                     start_at:
-#                     end_at:
-#                     specificities:
-#                     participants_min:
-#                     price_cents:
-#                     status:
-#                     course_id:
+# slot5 = Slot.create!(date: Date.today + 5,
+#                     start_at: DateTime.new((Date.today+5).year, (Date.today+5).month, (Date.today+5).day, 12, 00),
+#                     end_at: DateTime.new((Date.today+5).year, (Date.today+5).month, (Date.today+5).day, 13, 00),
+#                     specificities: "Des matelas seront sur place! Pensez juste à prendre des tenues confortables",
+#                     participants_min: 5,
+#                     price_cents: 10,
+#                     status: 0,
+#                     course_id: course1.id
 #                     )
 
-# puts "#{Slot.count} slots created"
+puts "#{Slot.count} slots created"
