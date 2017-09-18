dcd809e37202b2891212db654e71cf47a361ed09
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-09-18 10:47:20 +0200

    Relaunching ngrok

--- config/initializers/mangopay.rb
+++ config/initializers/mangopay.rb
@@ -10,7 +10,7 @@ MangoPay.configure do |c|
     if Rails.env.production?
       { host: "https://www.asanasano.com" }
     elsif Rails.env.development?
-      { host: "https://cfe52f2b.ngrok.io" }
+      { host: "https://20c6db74.ngrok.io" }
     end
   end
 
