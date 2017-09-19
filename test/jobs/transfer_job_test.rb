feb8ae08176ccfd57210015d637e4302b263f294
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-09-19 11:31:46 +0200

    Small transfer job test

--- test/jobs/transfer_job_test.rb
+++ test/jobs/transfer_job_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class TransferJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Good working of my TransferJob" do
+    # setup
+    travel_to Date.new(2017, 9, 8) do
+      # exercise
+      UpdateSlotStatusJob.perform_now
+      TransferJob.perform_now
+      # verify
+      assert_equal 2, MangopayLog.all.where(event: "transfer_creation").count
+      # teardown
+    end
+    after_teardown
+  end
 end
