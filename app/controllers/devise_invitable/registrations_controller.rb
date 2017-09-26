50df0979452d817a5a96b64ad8e9146177591c7f
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-09-26 15:42:10 +0200

    Adding CGU validations to registration process

---
+++ app/controllers/devise_invitable/registrations_controller.rb
@@ -0,0 +1,9 @@
+class DeviseInvitable::RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :agreed_to_terms)
+    end
+  end
+end
