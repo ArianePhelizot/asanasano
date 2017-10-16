e3624419e34be2caea488544b5c093602b4ee2f0
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-10-16 12:04:56 +0200

    Course intégration test

---
+++ test/integration/course_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class CourseTest < ActionDispatch::IntegrationTest
+  test "loads correctly" do
+    #set up
+    travel_to DateTime.new(2017, 9, 6, 8, 0) do
+      login_as users(:georges)
+      #exercice
+      visit course_path(Course.last)
+      #verify
+      assert_equal 200, page.status_code
+      assert page.has_content?("Amélie")
+      assert page.has_content?("jeudi 7 septembre")
+      assert page.has_content?("Patin")
+    end
+  end
+end
