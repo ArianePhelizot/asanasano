7f527c26ebc35f9ed85e8645f4f3251a2a5960f1
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-10-16 11:35:59 +0200

    Test integration on dashboard

---
+++ test/integration/dashboard_test.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class DashboardTest < ActionDispatch::IntegrationTest
+  test "loads correctly" do
+    #set up
+    travel_to DateTime.new(2017, 9, 6, 8, 0) do
+      login_as users(:georges)
+      #exercice
+      visit "/dashboard"
+      save_and_open_screenshot
+      #verify
+      assert_equal 200, page.status_code
+      assert page.has_content?("fraises")
+      assert page.has_content?("pommes")
+      assert page.has_content?("Patin")
+      assert page.has_content?("Amélie")
+    end
+  end
+end
