198c344ef42246733510fa4641d3cd8b721292e2
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-10-26 21:45:24 +0200

    Fix on profile page

--- test/integration/profile_test.rb
+++ test/integration/profile_test.rb
@@ -3,7 +3,7 @@
 require "test_helper"
 
 class ProfileTest < ActionDispatch::IntegrationTest
-  test "loads correctly" do
+  test "loads correctly when fully known Georges" do
     # set up
     login_as users(:georges)
     # exercice
@@ -12,5 +12,16 @@ class ProfileTest < ActionDispatch::IntegrationTest
     assert_equal 200, page.status_code
     assert page.has_content?("Georges")
     assert page.has_content?("Abitbol")
+  end
+
+    test "loads correctly when scarcely known Rocky" do
+    # set up
+    login_as users(:rocky)
+    # exercice
+    visit profile_path
+    # verify
+    assert_equal 200, page.status_code
+    assert page.has_content?("rocky@balboa.com")
+
   end
 end
