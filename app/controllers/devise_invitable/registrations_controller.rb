3a050b3f4fba4f0f31456c3098140446e6514fe2
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-10-29 20:32:36 +0100

    Special route from pro page to po registration

--- app/controllers/devise_invitable/registrations_controller.rb
+++ app/controllers/devise_invitable/registrations_controller.rb
@@ -6,4 +6,37 @@ class DeviseInvitable::RegistrationsController < Devise::RegistrationsController
       u.permit(:email, :password, :password_confirmation, :agreed_to_terms)
     end
   end
+
+  def newpro
+    new
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/MethodLength
+  def createpro
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        # respond_with resource, location: after_sign_up_path_for(resource)
+        redirect_to profile_path
+        # dashboard_path
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      # respond_with resource
+      render :newpro
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/MethodLength
 end
