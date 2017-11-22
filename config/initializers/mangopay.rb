76566710bd007864df76fa9d23ec44040109900e
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-11-22 17:43:11 +0100

    Mangopay new config on dev env

--- config/initializers/mangopay.rb
+++ config/initializers/mangopay.rb
@@ -10,7 +10,7 @@ MangoPay.configure do |c|
     if Rails.env.production?
       { host: "https://www.asanasano.com" }
     elsif Rails.env.development?
-      { host: "https://f1cfee27.ngrok.io" }
+      { host: "https://009b2f0b.ngrok.io" }
     end
   end
 
