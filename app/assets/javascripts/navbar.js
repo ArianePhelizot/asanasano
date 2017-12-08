3605585bbd3cae243325a13049fe5c2d6d7bd30c
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-12-08 16:43:58 +0100

    Adding new nav links

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,31 @@
+// $(function() {
+//     $('#employee').on('click',function(e){
+//         $('#employee').addClass('active');
+//         $('#enterprise').removeClass('active');
+//         $('#prof').removeClass('active');
+//     });
+    // $('#enterprise').on('click',function(e){
+    //     $('#enterprise').addClass('active');
+    //     $('#employee').removeClass('active');
+    //     $('#prof').removeClass('active');
+    // });
+    // $('#prof').on('click',function(e){
+    //     $('#prof').addClass('active');
+    //     $('#enterprise').removeClass('active');
+    //     $('#employee').removeClass('active');
+    // });
+// });
+
+
+// Event listnening seems to work but need to solve js errors first
+// $(function() {
+//     $('#employee').on('click',function(e){
+//         console.log("employee")
+//     });
+//     $('#enterprise').on('click',function(e){
+//         console.log("enterprise")
+//     });
+//     $('#prof').on('click',function(e){
+//         console.log("prof")
+//     });
+// });
