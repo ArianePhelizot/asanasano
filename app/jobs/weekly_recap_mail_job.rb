b6602e63ed70e15b2a2559d528053235b7127be0
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-12-10 19:16:20 +0100

    Weekly reminder fix

--- app/jobs/weekly_recap_mail_job.rb
+++ app/jobs/weekly_recap_mail_job.rb
@@ -4,25 +4,27 @@ class WeeklyRecapMailJob < ApplicationJob
 # rubocop:disable all
   def perform
 
-    # je sélectionne tous les users à qui sont proposés des activités
-    users_to_inform = []
+    unless Date.today.sunday?
+      # je sélectionne tous les users à qui sont proposés des activités
+      users_to_inform = []
 
 
-    # je regarde toutes les activités qui ont des activités avec des séances programmées
-    courses = Course.all
-    live_courses = courses.select { |course| course.next_slots.count.positive?}
+      # je regarde toutes les activités qui ont des activités avec des séances programmées
+      courses = Course.all
+      live_courses = courses.select { |course| course.next_slots.count.positive?}
 
-    # je regarde tous les utilisateurs qui pourraient réserver ces cours
-    live_courses.each do |course|
-      users_to_inform << course.group.users
-    end
+      # je regarde tous les utilisateurs qui pourraient réserver ces cours
+      live_courses.each do |course|
+        users_to_inform << course.group.users
+      end
 
-    # j'enlève les éventuels doublons
-    users_to_inform = users_to_inform.flatten.uniq
+      # j'enlève les éventuels doublons
+      users_to_inform = users_to_inform.flatten.uniq
 
-    # J'envoie un mail à chaque utilisateur
-    users_to_inform.each do |user|
-      UserMailer.weekly_recap_mail(user).deliver_now
+      # J'envoie un mail à chaque utilisateur
+      users_to_inform.each do |user|
+        UserMailer.weekly_recap_mail(user).deliver_now
+      end
     end
   end
 end
